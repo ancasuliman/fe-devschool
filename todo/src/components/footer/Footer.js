039d2c4d7183caa2393ac6a63c4e36df566da5ab
anca <anca.suliman@stud.acs.upb.ro>
2020-05-14 20:14:17 +0300

    display user location

--- todo/src/components/footer/Footer.js
+++ todo/src/components/footer/Footer.js
@@ -13,10 +13,66 @@ export class Footer extends LitElement {
     `;
   }
 
+  static get properties() {
+    return {
+      address: { type: Object },
+    };
+  }
+
+  constructor() {
+    super();
+
+    this.coordinates = null;
+    this.getLocation();
+  }
+
+  success(pos) {
+    const coordinates = {
+      latitude: pos.coords.latitude,
+      longitude: pos.coords.longitude,
+    };
+
+    this.getAddress(coordinates);
+  }
+
+  error(err) {
+    if (err.code === 1) {
+      alert('Error: Access is denied!');
+    } else if (err.code === 2) {
+      alert('Error: Position is unavailable!');
+    }
+  }
+
+  getLocation() {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.success.bind(this), this.error, options);
+    }
+  }
+
+  async getAddress(coordinates) {
+    let response = await fetch(
+      `https://nominatim.openstreetmap.org/reverse?format=json&
+          lat=${coordinates.latitude}&
+          lon=${coordinates.longitude}
+          `
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      this.address = data.display_name;
+    }
+  }
+
   render() {
     return html`
       <footer>
-        <p>LitElement To Do App &copy Anca Suliman</p>
+        <p>My location is ${this.address}</p>
       </footer>
     `;
   }
